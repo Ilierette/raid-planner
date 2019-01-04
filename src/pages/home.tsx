3577f81b665904bbc4182e89f22a4ee2543f2676
Karolina Pietraś <Ilierette@users.noreply.github.com>
2019-01-04 22:33:28 +0100

    webpack upgade

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -52,6 +52,18 @@ export default class Home extends React.Component<HomeState> {
     })
   }
 
+  returnHome = () => {
+    this.setState({
+      reload: false,
+      name: "Letty"
+    })
+    setTimeout(()=>{
+      this.setState({
+        reload: true,
+      })
+    }, 1);
+  }
+
   render() {
     return (
       <div className="content-wrapper">
@@ -59,18 +71,25 @@ export default class Home extends React.Component<HomeState> {
         <div className="content">
           <div className="card bg-dark">
             <div className="card-body">
-              <div className="char-data mx-auto px-5">
-                <form className="form-inline mb-2" onSubmit={(e: any) => this.handleSubmit(e)}>
-                  <div className="form-group">
-                    <CharacterSelectDropdown />
-                    <input placeholder="Find other character" className="form-control ml-1 mr-1 bg-dark text-light" onKeyUp={(e: any) => this.changeName(e)} />
-                    <select className="form-control mr-3 bg-dark text-light" onChange={(e: any) => this.changeRegion(e)}>
-                      <option value="eu">EU</option>
-                      <option value="na">NA</option>
-                    </select>
-                  </div>
-                  <button className="btn btn-outline-primary">Search</button>
-                </form>
+              <div className="char-data row mx-auto">
+                <div className="col-1">
+                  <button className="btn btn-outline-primary" onClick={()=>this.returnHome()}>
+                    <FontAwesomeIcon icon="home" />
+                  </button>
+                </div>
+                <div className="col-11 reversed">
+                  <form className="form-inline mb-2" onSubmit={(e: any) => this.handleSubmit(e)}>
+                    <div className="form-group">
+                      <CharacterSelectDropdown />
+                      <input placeholder="Find other character" className="form-control ml-1 mr-1 bg-dark text-light" onKeyUp={(e: any) => this.changeName(e)} />
+                      <select className="form-control mr-3 bg-dark text-light" onChange={(e: any) => this.changeRegion(e)}>
+                        <option value="eu">EU</option>
+                        <option value="na">NA</option>
+                      </select>
+                    </div>
+                    <button className="btn btn-outline-primary">Search</button>
+                  </form>
+                </div>
               </div>
 
               {this.state.reload ?
@@ -79,63 +98,64 @@ export default class Home extends React.Component<HomeState> {
                   <div className="loader"></div>
                 </div>
               }
+              {this.state.reload &&
+                <div className="char-details row mx-auto">
+                  <div className="col-6 mt-2">
+                    <div className="card text-center text-white bg-dark border-primary">
+                      <div className="card-body">
+                        <h5>DPS Parse</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{this.state.dpsCount}</h6>
+                        <a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Update parse</a>
+                      </div>
+                      <img className="card-img-bottom" src={this.state.dpsImg} />
+                    </div>
+                  </div>
+                  <div className="col-6 mt-2">
+                    <div className="card text-center text-white bg-dark border-primary">
+                      <div className="card-body">
+                        <h5>Needs</h5>
+                        <ul className="list-unstyled">
+                          <li>TT Ring  <span className="badge badge-success ml-1 my-auto">awakened</span> </li>
+                          <li>TT Earring</li>
+                          <li><a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Add EQ</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-6 mt-2">
+                    <div className="card text-center text-white bg-dark border-primary">
+                      <div className="card-body">
+                        <h5>Raid member</h5>
+                        <ul className="list-unstyled">
+                          <li><a href="" >TT Raid List <span className="badge badge-primary ml-1 my-auto">static</span> </a></li>
+                          <li><a href="" >VT Raid List <span className="badge badge-secondary ml-1 my-auto">sub</span> </a></li>
+                        </ul>
 
-              <div className="char-details row mx-auto">
-                <div className="col-6 mt-2">
-                  <div className="card text-center text-white bg-dark border-primary">
-                    <div className="card-body">
-                      <h5>DPS Parse</h5>
-                      <h6 className="card-subtitle mb-2 text-muted">{this.state.dpsCount}</h6>
-                      <a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Update parse</a>
-                    </div>
-                    <img className="card-img-bottom" src={this.state.dpsImg} />
-                  </div>
-                </div>
-                <div className="col-6 mt-2">
-                  <div className="card text-center text-white bg-dark border-primary">
-                    <div className="card-body">
-                      <h5>Needs</h5>
-                      <ul className="list-unstyled">
-                        <li>TT Ring  <span className="badge badge-success ml-1 my-auto">awakened</span> </li>
-                        <li>TT Earring</li>
-                        <li><a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Add EQ</a></li>
-                      </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="col-6 mt-2">
-                  <div className="card text-center text-white bg-dark border-primary">
-                    <div className="card-body">
-                      <h5>Raid member</h5>
-                      <ul className="list-unstyled">
-                        <li><a href="" >TT Raid List <span className="badge badge-primary ml-1 my-auto">static</span> </a></li>
-                        <li><a href="" >VT Raid List <span className="badge badge-secondary ml-1 my-auto">sub</span> </a></li>
-                      </ul>
-
+                  <div className="col-6 mt-2">
+                    <div className="card text-center text-white bg-dark border-primary">
+                      <div className="card-body">
+                        <h5>Raid leader</h5>
+                        <ul className="list-unstyled">
+                          <li><a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Add raid</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 mt-2">
+                    <div className="card text-center text-white bg-dark border-primary">
+                      <div className="card-body">
+                        <h5>Timetable</h5>
+                        <ul className="list-unstyled">
+                          <li><a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Edit timetable</a></li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="col-6 mt-2">
-                  <div className="card text-center text-white bg-dark border-primary">
-                    <div className="card-body">
-                      <h5>Raid leader</h5>
-                      <ul className="list-unstyled">
-                        <li><a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Add raid</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-12 mt-2">
-                  <div className="card text-center text-white bg-dark border-primary">
-                    <div className="card-body">
-                      <h5>Timetable</h5>
-                      <ul className="list-unstyled">
-                        <li><a href="" ><FontAwesomeIcon icon="plus" className="mr-1" /> Edit timetable</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              }
             </div>
           </div>
         </div>
